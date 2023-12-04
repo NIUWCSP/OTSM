720f743f1189c481fcc23787e7338c909c500865
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2023-12-05 00:11:45 +0800

    初始程式

---
+++ Generate_Matched_Filter_GS_matrices.m
@@ -0,0 +1,27 @@
+function [Gn_block_matrix,Tn_block_matrix,zn_block_vector,H_t_f]=Generate_Matched_Filter_GS_matrices(N,M,G,r)
+
+% Generate time-domain GS iteration matrices for low complexity iterative detection
+Gn_block_matrix=zeros(M,M,N);
+Tn_block_matrix=zeros(M,M,N);
+Qn_block_matrix=zeros(M,M,N);
+zn_block_vector=zeros(M,N);
+
+H_t_f=zeros(N,M); % Time-frequency single tap channel matrix
+Fn=dftmtx(M);
+Fn=Fn./norm(Fn);
+for n=1:N
+    rn=r((n-1)*M+1:n*M);
+    Gn_block_matrix(:,:,n)=G((n-1)*M+1:n*M,(n-1)*M+1:n*M);
+    Gn=Gn_block_matrix(:,:,n);
+    H_t_f(n,1:M)=diag(Fn*Gn*Fn').';  % Generate time-frequency channel matrix for low complexity initial estimate using equation (20) in [R2]
+    Rn=Gn'*Gn;    
+    Dn=diag(diag(Rn));
+    Ln=tril(Rn,-1);
+    Un=triu(Rn,1);
+    Qn=(Dn+Ln)^(-1);
+    Tn=Qn*Un;
+    Tn_block_matrix(:,:,n)=Tn;
+    Qn_block_matrix(:,:,n)=Qn;        
+    zn_block_vector(:,n)=Qn*Gn'*rn;
+end
+end
