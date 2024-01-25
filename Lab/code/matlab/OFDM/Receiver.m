60d1b7050160af555f770ade7fa7ad108763c925
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-01-25 13:13:34 +0800

    Recivier基本參數設置+督普勒參數

--- Lab/code/matlab/OFDM/Receiver.m
+++ Lab/code/matlab/OFDM/Receiver.m
@@ -9,6 +9,17 @@ NumDataOfdmSymb = 18;
 NumDataSubcarrier = 108;
 RxSignalExt(:,1)=RxSignal;
 figure(2);clf;
+
+%% OTFS parameters%%%%%%%%%%
+% N: number of symbols in time
+N = 64;
+% M: number of subcarriers in frequency
+M = 64;
+% Time and frequency resources
+car_fre = 4*10^9;% Carrier frequency
+delta_f = 15*10^3; % subcarrier spacing: 15 KHz
+T = 1/delta_f; %one time symbol duration in OTFS frame
+
 %% Receiver
 NumSyncSymb = NumSyncPreamble*2 + NumFFT;
 NumPilotSymb = NumFFT * 2;
@@ -30,6 +41,13 @@ RxSignalRadioFrame = RxSignalExt(StartIdx:StartIdx+NumRadioFrame-1);
 
 % Pilot OFDM symbol
 PilotOfdmSymb = reshape(RxSignalRadioFrame(NumSyncSymb+1:NumSyncSymb+NumPilotSymb), [], 2);
+
+ % 3GPP channel model
+ max_speed=500;  % km/hr
+[chan_coef,delay_taps,Doppler_taps,taps]=Generate_delay_Doppler_channel_parameters(N,M,car_fre,delta_f,T,max_speed);
+
+ %% channel output%%%%% 
+[G,gs]=Gen_time_domain_channel(N,M,taps,delay_taps,Doppler_taps,chan_coef);
 
 %% Demodulation
 % Estimate carrier frequency offset
