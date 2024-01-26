0f796a6735c0921ccc2aa0b3839a2f5b3883887c
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-01-26 14:16:01 +0800

    Receiver加入BER (刪除舊檔後)

--- Lab/code/matlab/OFDM/Receiver.m
+++ Lab/code/matlab/OFDM/Receiver.m
@@ -1,15 +1,5 @@
 function RxDataBits = Receiver(RxSignal)
 
-% Setting parameters
-NumFFT = 128; %V3
-NumSyncPreamble = 32; %V3
-NumCP = 16; %V3
-
-NumDataOfdmSymb = 18;
-NumDataSubcarrier = 108;
-RxSignalExt(:,1)=RxSignal;
-figure(2);clf;
-
 %%廣域變數宣告
 global TxDataBits;
 global set_looptimes;
@@ -70,28 +60,7 @@ Wn=Wn./norm(Wn);  % normalize the WHT matrix
 for iesn0 = 1:set_looptimes
     for ifram = 1:N_fram
          current_frame_number(iesn0)=ifram;
-        %% Receiver
-        NumSyncSymb = NumSyncPreamble*2 + NumFFT;
-        NumPilotSymb = NumFFT * 2;
-        NumDataSymb = (NumFFT+NumCP) * NumDataOfdmSymb;
-        NumRadioFrame = NumSyncSymb + NumPilotSymb + NumDataSymb;
-        
-        StartIdx = SyncRxSignalImproved1(RxSignalExt, 1, NumFFT);
-        
-        %%JF加入重新賦值=1避免StartIdx == -1時直接中斷程式：
-        global NoFoundDataTimes;
-        if(StartIdx == -1)
-            StartIdx = 1;
-            NoFoundDataTimes=NoFoundDataTimes+1;
-        elseif(StartIdx+NumRadioFrame-1 >= 31680)
-            StartIdx = 1;
-            NoFoundDataTimes=NoFoundDataTimes+1;
-        end
-        RxSignalRadioFrame = RxSignalExt(StartIdx:StartIdx+NumRadioFrame-1);
-        
-        % Pilot OFDM symbol
-        PilotOfdmSymb = reshape(RxSignalRadioFrame(NumSyncSymb+1:NumSyncSymb+NumPilotSymb), [], 2);
-        
+
          %% OTFS channel generation%%%%
          % 3GPP channel model
          max_speed=500;  % km/hr
@@ -108,33 +77,7 @@ for iesn0 = 1:set_looptimes
                             r(q+1)=r(q+1)+gs(l+1,q+1)*RxSignal(q-l+1);  %equation (24) in [R1]
                         end
                     end
-                end
-        %% Demodulation
-        % Estimate carrier frequency offset
-        RxPilotSymb = OfdmSignalDemodulation(PilotOfdmSymb, NumFFT, 0, NumDataSubcarrier);
-        XCorrPilot = RxPilotSymb(:,1)' * RxPilotSymb(:,2);
-        EpsEst = 1/(2*pi) * atan(imag(XCorrPilot)/real(XCorrPilot));
-        
-        % Estimate carrier freqnecy offset
-        RxSigalRadioFrameCmpCFO = RxSignalRadioFrame .* ...
-            exp(-1j*2*pi*EpsEst/NumFFT * (0:length(RxSignalRadioFrame)-1)');
-        
-        % Reobtain pilot data
-        PilotOfdmSymb = reshape( ...
-            RxSigalRadioFrameCmpCFO(NumSyncSymb+1:NumSyncSymb+NumPilotSymb), [], 2);
-        % Data OFDM symbol
-        DataOfdmSymb = reshape( ...
-            RxSigalRadioFrameCmpCFO(NumSyncSymb+NumPilotSymb+1:end), ...
-            [], NumDataOfdmSymb);
-        
-        RxPilotSymb = OfdmSignalDemodulation(PilotOfdmSymb, NumFFT, 0, NumDataSubcarrier);
-        RxDataSymb = OfdmSignalDemodulation(DataOfdmSymb, NumFFT, NumCP, NumDataSubcarrier);
-        
-        % Channel estimation and equalization
-        BpskModObj = comm.BPSKModulator('PhaseOffset', pi/4);
-        PilotBits = GetPilotBits();
-        TxPilotSymb = step(BpskModObj,PilotBits);
-        ChanEst = RxPilotSymb(:,1) ./ TxPilotSymb;
+                 end
         
         %% OTSM demodulation%%%%
                 Y_tilda=reshape(r,M,N);     %equation (11) in [R1]
@@ -190,17 +133,4 @@ for iesn0 = 1:set_looptimes
         
     end  
 end
-global RxDataSymbEq;
-RxDataSymbEq = RxDataSymb ./ repmat(ChanEst, 1, NumDataOfdmSymb);
-subplot(232);plot(10*log10(abs(ChanEst).^2)-min(10*log10(abs(ChanEst).^2)));title('channel estimation');
-subplot(233);plot(RxDataSymb(:),'*');axis equal;title('');title('scatter before equalization');axis square;
-subplot(234);plot(RxDataSymbEq(:).*exp(-1i*pi/4),'.');axis([-1.5,1.5,-1.5,1.5]);title('scatter after equalization'); axis square;
-% Demodulation
-BpskDemodulatorObj = comm.BPSKDemodulator( ...
-    'PhaseOffset', pi/4, ...
-    'DecisionMethod', 'Hard decision');
-global RxDataBits;
-RxDataBits = zeros(size(RxDataSymbEq));
-for idx = 1:NumDataOfdmSymb
-    RxDataBits(:,idx) = step(BpskDemodulatorObj,RxDataSymbEq(:,idx));
-end
+
