080fee07cd6574b2377e5f8e9333bab667cee615
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-03-07 16:18:48 +0800

    通道估測(未完成)

--- Lab/code/matlab/OFDM/Receiver.m
+++ Lab/code/matlab/OFDM/Receiver.m
@@ -87,33 +87,47 @@ RxSignalExt(:,1)=RxSignal;
          %% channel output%%%%% 
         [G,gs]=Gen_time_domain_channel(N,M,taps,delay_taps,Doppler_taps,chan_coef);
 
-         r=zeros(N*M,1);
-         noise= sqrt(sigma_2(iesn0)/2)*(randn(size(RxSignalRadioFrame)) + 1i*randn(size(RxSignalRadioFrame)));
-         l_max=max(delay_taps);
-         for q=0:N*M-1
-           for l=0:l_max
-             if(q>=l)
-               r(q+1)=r(q+1)+gs(l+1,q+1)*RxSignalRadioFrame(q-l+1);  %equation (24) in [R1]
-             end
-           end
-         end
-         r=r+noise;
+         %% OTSM Reobtain Pilot%%%%
+                
+         %Pilot side
+         RxSignalGrid=reshape(RxSignalRadioFrame,N,M);
+         Y_OTSM_Pilot=RxSignalGrid(M_data+sqrt(PilotSymb)+1:M_data+sqrt(PilotSymb)*2,1:sqrt(PilotSymb));%傳送資料時Pilot和Sync皆以8*8的形式傳送
+
+        % Estimate carrier frequency offset
+XCorrPilot = Y_OTSM_Pilot(:,1)' * Y_OTSM_Pilot(:,2);%導頻符號的交叉相關
+EpsEst = 1/(2*pi) * atan(imag(XCorrPilot)/real(XCorrPilot));%頻率偏移的估計
+
+% Estimate carrier freqnecy offset
+RxSigalRadioFrameCmpCFO = RxSignalRadioFrame .* ...
+    exp(-1j*2*pi*EpsEst/NumFFT * (0:length(RxSignalRadioFrame)-1)');%接收訊號進行CFO校正
+RxSignalRadioGridCFO = reshape(RxSigalRadioFrameCmpCFO,N,M);
+
+% Reobtain pilot data
+PilotOtsmSymb = RxSignalRadioGridCFO(M_data+sqrt(PilotSymb)+1:M_data+sqrt(PilotSymb)*2,1:sqrt(PilotSymb));%將所選的一段導頻資料重新組織成一個矩陣，其中每列有兩個元素
+
+% Data OFDM symbol
+DataOtsmSymb = RxSignalRadioGridCFO(1:M_data,1:M);
+
+
+% Channel estimation and equalization
+PilotBits = GetPilotBits();
+TxPilotSymb = qammod(reshape(PilotBits,[],1),M_mod,'gray','OutputType','bit');%產生Tx的 BPSK符號序列
+ChanEst = RxPilotSymb(:,1) ./ TxPilotSymb;%通道估计
+global RxDataSymbEq;
+RxDataSymbEq = RxDataSymb ./ repmat(ChanEst, 1, NumDataOfdmSymb);
+subplot(232);plot(10*log10(abs(ChanEst).^2)-min(10*log10(abs(ChanEst).^2)));title('channel estimation');%繪製通道估計的幅度譜
+subplot(233);plot(RxDataSymb(:),'*');axis equal;title('scatter before equalization');axis square;
+subplot(234);plot(RxDataSymbEq(:).*exp(-1i*pi/4),'.');axis([-1.5,1.5,-1.5,1.5]);title('scatter after equalization'); axis square;%*exp(-1i*pi/4) 的作用是進行相位調整
 
         %% OTSM demodulation%%%%
                 
                 Y_tilda=reshape(RxSignalRadioFrame,M,N);     %equation (11) in [R1]
                 Y = Y_tilda*Wn;             %equation (12) in [R1]
                
-
-        %% OTSM Reobtain Pilot%%%%
-                
-                %Pilot side
-                Y_Pilot=reshape(Y(M_data+sqrt(PilotSymb)+1:M_data+sqrt(PilotSymb)*2,1:sqrt(PilotSymb)),[],1);%傳送資料時Pilot和Sync皆以8*8的形式傳送
-                Pilot_ErrorBits=sum(xor(qamdemod(Y_Pilot,M_mod,'gray','OutputType','bit'),reshape(GetPilotBits,[],1)));
          
         
         %% Generate the block-wise channel matrices in the delay-time and the time-frequency domain
-        [Gn_block_matrix,Tn_block_matrix,zn_block_vector,H_t_f]=Generate_Matched_Filter_GS_matrices(N,M,G,r);
+        [Gn_block_matrix,Tn_block_matrix,zn_block_vector,H_t_f]=Generate_Matched_Filter_GS_matrices(N,M,G,RxSignalRadioFrame);
         
          %% GS SOR Iterative detection
         
@@ -123,8 +137,8 @@ RxSignalExt(:,1)=RxSignal;
             omega=0.25;
         end
         decision=1; %1-hard decision, 0-soft decision
-        [est_info_bits_MFGS,det_iters_MFGS,data_MFGS] = Matched_Filter_GS_detector(N,M,M_mod,sigma_2(iesn0),data_grid,Y,H_t_f,n_ite_MRC,omega,Tn_block_matrix,Gn_block_matrix,zn_block_vector,r,Wn,decision);
+        [est_info_bits_MFGS,det_iters_MFGS,data_MFGS] = Matched_Filter_GS_detector(N,M,M_mod,sigma_2(iesn0),data_grid,Y,H_t_f,n_ite_MRC,omega,Tn_block_matrix,Gn_block_matrix,zn_block_vector,RxSignalRadioFrame,Wn,decision);
         [est_info_bits_1tap,data_1tap] = TF_single_tap_equalizer(N,M,M_mod,sigma_2(iesn0),data_grid,Y,H_t_f,Wn);
-        [est_info_bits_LMMSE,data_LMMSE] = Block_LMMSE_detector(N,M,M_mod,sigma_2(iesn0),data_grid,Gn_block_matrix,r,Wn);
+        [est_info_bits_LMMSE,data_LMMSE] = Block_LMMSE_detector(N,M,M_mod,sigma_2(iesn0),data_grid,Gn_block_matrix,RxSignalRadioFrame,Wn);
         RxDataBits=0;
        
