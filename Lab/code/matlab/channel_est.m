ddeff45aa62e9839f1e3d59929399e7fe9bcef74
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-03-26 17:09:35 +0800

    CFO程式碼修正
    陳昱升

--- Lab/code/matlab/channel_est.m
+++ Lab/code/matlab/channel_est.m
@@ -18,21 +18,25 @@ EpsEst = 1/(2*pi) * atan(imag(XCorrPilot)/real(XCorrPilot));%頻率偏移的估�
 
 %測試用CFO
 RxSignalRadioGrid=reshape(RxSignalRadioFrame,N,M);
-y_mp1=zeros(size(Y_OTSM_Pilot,1),1); %64*1
-y_mp2=zeros(size(Y_OTSM_Pilot,1),1); %64*1
+y_mp1=zeros(size(Y_OTSM_Pilot,1),l_max+1); %64*1
+y_mp2=zeros(size(Y_OTSM_Pilot,1),l_max+1); %64*1
+N_p2=sqrt(size(Y_OTSM_Pilot,1));
 
   for l=0:l_max
       for n=1:sqrt(size(Y_OTSM_Pilot,1)) %1~8
           for mp1=M_data+1:M_data+sqrt(size(Y_OTSM_Pilot,1)) %41~48
-                    y_mp1((mp1-M_data)+(n-1)*8,1)=gs(l+1,mp1+l+n*M+1).* exp(1j*2*pi*EpsEst/M*(mp1+l+n*M+1))*RxSignalRadioGrid(mp1,n);
+                    y_mp1((mp1-M_data)+(n-1)*8,l+1)=gs(l+1,mp1+l+n*M+1).* exp(1j*2*pi*EpsEst/M*(mp1+l+n*M+1))*RxSignalRadioGrid(mp1,n);
           end
-          for mp2=M_data+sqrt(size(Y_OTSM_Pilot,1))+1:M_data+sqrt(size(Y_OTSM_Pilot,1))*2 %49~56
-                    y_mp2((mp2-M_data-sqrt(size(Y_OTSM_Pilot,1)))+(n-1)*8,1)=exp(1j*pi/2)*gs(l+1,mp2+l+n*M+1).* exp(1j*2*pi*EpsEst/M*(mp2+l+n*M+1))*RxSignalRadioGrid(mp2,n);
+          for mp2=M_data+N_p2+1:M_data+sqrt(size(Y_OTSM_Pilot,1))*2 %49~56
+                    y_mp2((mp2-M_data-N_p2)+(n-1)*8,l+1)=exp(1j*pi/2)*gs(l+1,mp2+l+n*M+1).* exp(1j*2*pi*EpsEst/M*(mp2+l+n*M+1))*RxSignalRadioGrid(mp2,n);
           end
       end
   end %%equation (17) in [R3]
-XCorrmp = y_mp1(:,1) * (exp(1j*pi/2)*y_mp2(:,1)');
-tilda_CFO = -M/(2*pi*l_max*length(delay_taps))* atan(imag(XCorrmp)/real(XCorrmp)); %%equation (18) in [R3]
+    tilda_CFO=0; %初始化
+  for l=0:l_max
+    XCorrmp=y_mp1(:,l+1) * (exp(1j*pi/2)*y_mp2(:,l+1)');
+    tilda_CFO = tilda_CFO+(-M/(2*pi*l_max*length(delay_taps))* atan(imag(XCorrmp)/real(XCorrmp))); %%equation (18) in [R3]
+  end
 
 global NoFoundDataTimes;
 if (isnan(atan(imag(XCorrmp)/real(XCorrmp))))
