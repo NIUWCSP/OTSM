a895e90dafa6d3d7b94dedabe1326ed7e83bb68f
NIUWCSP <121147600+NIUWCSP@users.noreply.github.com>
2024-04-03 16:15:15 +0800

    .

--- Lab/code/matlab/OTSM_demo_tx.m
+++ Lab/code/matlab/OTSM_demo_tx.m
@@ -87,6 +87,34 @@ for iesn0 = 1:length(SNR_dB)
         input{s.getInChannel('TX_LO_FREQ')} = 2400e6;
         input{s.getInChannel('TX_SAMPLING_FREQ')} = 40e6;
         input{s.getInChannel('TX_RF_BANDWIDTH')} = 20e6;
+% %% Transmit and Receive using MATLAB libiio
+% 
+% % System Object Configuration
+% s = iio_sys_obj_matlab; % MATLAB libiio Constructor
+% s.ip_address = ip;
+% s.dev_name = 'ad9361';
+% s.in_ch_no = 2;
+% s.out_ch_no = 2;
+% s.in_ch_size = length(txdata);
+% s.out_ch_size = length(txdata).*10;
+% 
+% s = s.setupImpl();
+% 
+% input = cell(1, s.in_ch_no + length(s.iio_dev_cfg.cfg_ch));
+% output = cell(1, s.out_ch_no + length(s.iio_dev_cfg.mon_ch));
+% 
+% % Set the attributes of AD9361
+% input{s.getInChannel('RX_LO_FREQ')} = 2400e6;
+% input{s.getInChannel('RX_SAMPLING_FREQ')} = 40e6;
+% input{s.getInChannel('RX_RF_BANDWIDTH')} = 20e6;
+% input{s.getInChannel('RX1_GAIN_MODE')} = 'manual';%% slow_attack manual
+% input{s.getInChannel('RX1_GAIN')} = 1;
+% input{s.getInChannel('TX_LO_FREQ')} = 2400e6;
+% input{s.getInChannel('TX_SAMPLING_FREQ')} = 40e6;
+% input{s.getInChannel('TX_RF_BANDWIDTH')} = 20e6;
+% %改gain
+% %txPluto = sdrtx('Pluto','Gain',0)
+
     
         a=0;%% 輸出迴圈次數
     
@@ -94,11 +122,15 @@ for iesn0 = 1:length(SNR_dB)
         fprintf('Transmitting Data Block %i ...\n',i);
         input{1} = real(txdata);
         input{2} = imag(txdata);
-        output = writeTxData(s, input);%調用pluto的通道資料
+        writeTxData(s, input);%調用pluto的通道資料
         a=a+1;
         fprintf('%d\n',a);
         fprintf('Data Block %i Received...\n',i);
-    end
+    end    
+    i=0;
+    %while沒加
+    
+
 %     I = output{1};
 %     Q = output{2};
 %     Rx = I+1i*Q;
